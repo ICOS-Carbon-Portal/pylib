aa5ee0f1fa4f2d215d56a96b9065e61dc122c3f3
ZogopZ <zoiszogop@gmail.com>
2022-06-28 14:55:59 +0200

    zz release 0.1.15
    - Generate 0.1.15 distribution files.
    - Update files to the new release version 0.1.15.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def readme():
 
 setuptools.setup(
     name='icoscp',
-    version='0.1.14',
+    version='0.1.15',
 	license='GPLv3+',
     author="Claudio D'Onofrio, Zois Zogopoulos, ICOS Carbon Portal",
     author_email='claudio.donofrio@nateko.lu.se, zois.zogopoulos@nateko.lu.se, info@icos-cp.eu',
