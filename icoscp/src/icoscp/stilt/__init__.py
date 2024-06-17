cb28ad9a6d839fe788a5012df02a3143a321135c
zoiszogop@gmail.com <zoiszogop@gmail.com>
2024-06-17 12:15:18 +0200

    icoscp - convert ImportWarning to UserWarning to avoid extra jupyter errors

--- icoscp/src/icoscp/stilt/__init__.py
+++ icoscp/src/icoscp/stilt/__init__.py
@@ -1,9 +1,9 @@
 import warnings
 
-warnings.simplefilter('always', ImportWarning)
+warnings.simplefilter('always', UserWarning)
 stilt_warning = (
     "\nThe icoscp STILT module has been moved to a new dedicated library: "
     "https://icos-carbon-portal.github.io/pylib/icoscp_stilt/\n"
     'To import it use: "from icoscp_stilt import stilt"'
 )
-warnings.warn(stilt_warning, ImportWarning, stacklevel=2)
+warnings.warn(stilt_warning, UserWarning, stacklevel=2)
