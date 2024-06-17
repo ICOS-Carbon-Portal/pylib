018120e925e41b706a97feab7a355d33a14a7ec0
zoiszogop@gmail.com <zoiszogop@gmail.com>
2024-06-17 11:58:29 +0200

    icoscp - add warnings when importing the old dobj & stilt modules

--- icoscp/src/icoscp/cpb/__init__.py
+++ icoscp/src/icoscp/cpb/__init__.py
@@ -1,8 +1,20 @@
 """
-    The cpb module gives you access to a binary representatin of 
-    digital data objects hosted at the ICOS CarbonPortal.
+    The cpb module gives you access to a binary representation of
+    digital data objects hosted at the ICOS Carbon Portal.
     Any data column (x,y axis) you can "preview" on the data portal
     ( https://data.icos-cp.eu/portal/ ) is accessible from this module.
     You need to know the digital object ID (URL, persistent identification)
-    or have a sqparql query providing this information.    
+    or have a SPARQL query providing this information.
 """
+import warnings
+
+warnings.simplefilter('always', UserWarning)
+old_dob_warning = (
+    "\nIt is highly recommended to replace the following import:\n"
+    '"from icoscp.cpb.dobj import Dobj"\n'
+    "  with\n"
+    '"from icoscp.dobj import Dobj"\n'
+    "Find out more here: "
+    "https://icos-carbon-portal.github.io/pylib/icoscp/install/#upgrade-guide"
+)
+warnings.warn(old_dob_warning, UserWarning, stacklevel=2)
---
+++ icoscp/src/icoscp/stilt/__init__.py
@@ -0,0 +1,9 @@
+import warnings
+
+warnings.simplefilter('always', ImportWarning)
+stilt_warning = (
+    "\nThe icoscp STILT module has been moved to a new dedicated library: "
+    "https://icos-carbon-portal.github.io/pylib/icoscp_stilt/\n"
+    'To import it use: "from icoscp_stilt import stilt"'
+)
+warnings.warn(stilt_warning, ImportWarning, stacklevel=2)
