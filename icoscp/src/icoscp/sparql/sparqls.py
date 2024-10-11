316df99adae0a16d280d84e91843f1f63edbad59
Oleg Mirzov <oleg.mirzov@nateko.lu.se>
2024-10-11 10:57:42 +0200

    station - make the station list query change minimal

--- icoscp/src/icoscp/sparql/sparqls.py
+++ icoscp/src/icoscp/sparql/sparqls.py
@@ -460,10 +460,9 @@ def station_query(filter: dict = None, return_filter: bool = False) -> str or (s
                     {theme_filter}
                     ?uri a ?stationTheme .
                     OPTIONAL{{
-                        ?memb cpmeta:atOrganization ?uri .
+                        ?memb cpmeta:atOrganization ?uri ; cpmeta:hasRole <http://meta.icos-cp.eu/resources/roles/PI> .
                         optional {{?memb cpmeta:hasEndTime ?membEnd}}
                         filter(!bound(?membEnd) || ?membEnd > now())
-                        ?memb cpmeta:hasRole <http://meta.icos-cp.eu/resources/roles/PI> .
                         ?pers cpmeta:hasMembership ?memb
                     }}
                 }}
